f0f7485d253ed388ec13782a9e15c4023db14b90
Viktoriya Danilova <v_kozulya@mail.ru>
2022-11-08 00:01:52 +0500

    feat: finish task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+
+function copyDir() {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Directory has been created!');
+    });
+    try {
+        fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, list) => {
+            if (err)
+                console.log(err);
+            else {
+                list.forEach(file => {
+                    fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name),  err => {
+                        if (err) console.log(err);
+                    })
+                })
+            }
+        })
+    } catch (err) {
+        console.error(err);
+    }
+    console.log('Directory has been copied!');
+}
+
+copyDir();
+
+    
 
