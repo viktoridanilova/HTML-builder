eab2531015a940ae957b33a5d4164d18994bf2af
Viktoriya Danilova <v_kozulya@mail.ru>
2022-11-08 00:01:35 +0500

    feat: finish task

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+// const process = require('process');
+
+const write = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout,
+})
+
+function ask(question) {
+    rl.question(question, (answer) => {
+        if(answer === "exit") {
+            process.exit(1)
+        }
+        fs.appendFile(`${__dirname}/text.txt`, answer, function(error){
+          if(error) throw error; 
+          console.log('The data was successfully written to the file!');
+        });
+
+        ask(question)
+    })
+}
+
+ask("Enter text: ") 
+
+// fs.appendFile((write), answer, err => {
+//   if(err) {
+//     console.log(err);
+//     return;
+//   }
+// })
